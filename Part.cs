d12dc7037a6798f2d494a88353d6e10ecd50afc3
Majiir Paktu <majiir@nabaal.net>
2012-03-07 20:35:19 -0500

    Initial commit

---
+++ Part.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace KerbalAPI
+{
+    public abstract partial class Part<T> where T : Part<T>, new()
+    {
+
+        #region Proxy Mechanism
+
+        private Proxy _proxy;
+
+        private Proxy proxy
+        {
+            get
+            {
+                if (_proxy == null)
+                {
+                    throw new Exception("Part proxy not assigned! (Are you trying to construct a Part manually?)");
+                }
+                return _proxy;
+            }
+            set { _proxy = value; }
+        }
+
+        private static Part<T> Create(Proxy proxy)
+        {
+            Part<T> part = new T();
+            part.proxy = proxy;
+            return part;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Explode()
+        {
+            proxy.explode();
+        }
+
+        #endregion
+
+        #region Events
+
+        public event EventHandler ActiveUpdate;
+
+        protected virtual void OnActiveUpdate()
+        {
+            EventHandler handler = ActiveUpdate;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+
+        public event EventHandler Start;
+
+        protected virtual void OnStart()
+        {
+            EventHandler handler = Start;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ PartProxy.cs
@@ -0,0 +1,38 @@
+﻿
+namespace KerbalAPI
+{
+    public abstract partial class Part<T> where T : Part<T>, new()
+    {
+        public abstract class Proxy : global::Part
+        {
+
+            #region Proxy Mechanism
+
+            private Part<T> part;
+
+            public Proxy() : base()
+            {
+                part = Part<T>.Create(this);
+            }
+
+            #endregion
+
+            #region Events
+
+            protected override void onPartStart()
+            {
+                base.onPartStart();
+                part.OnStart();
+            }
+
+            protected override void onActiveUpdate()
+            {
+                base.onActiveUpdate();
+                part.OnActiveUpdate();
+            }
+
+            #endregion
+
+        }
+    }
+}
